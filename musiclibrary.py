7e4d5715f7f6ab7cb6cd23127f27b88f1fd84de4
Tanzeelur Rahman <157966219+Zarrar201@users.noreply.github.com>
2025-08-08 03:37:25 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "man mast malang ost": "https://youtu.be/J-pbbK4ld80?si=Ncim0CQUW82nf7T_",
+    "agency" : "https://youtu.be/42Ekv1x_Qdo?si=nknpykavtGRg1Y1E",
+    "bandish": "https://youtu.be/8L3lEJV-BeU?si=GBZJwEkUbLa3_nzH",
+    "manzarkashi": "https://youtu.be/rx5JrTiJYEs?si=4OS_GZCbMOPueEOI"
+}
